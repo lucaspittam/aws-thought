fddb414f6a08c107ba636f44ee734be76a793413
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-08-11 13:15:07 -0400

    integrate API endpoints into front end/ deployed on aws through dynamodb

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 const userRoutes = require('./routes/user-routes');
 
 // express middleware, used to be bodyparser
